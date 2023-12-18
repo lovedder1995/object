11b8d7a8af0473602be36b2d10e7704453d1c339
Edder Alberto López Villastrigo <lovedder1995@outlook.com>
2023-12-17 23:43:00 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+# github:lovedder1995/object#1.0.0
+
+return function (...$properties) {
+    return $properties;
+};
---
+++ main.php
@@ -0,0 +1,4 @@
+# github:lovedder1995/object#1.0.0
+
+return function (...$properties)
+    return $properties
